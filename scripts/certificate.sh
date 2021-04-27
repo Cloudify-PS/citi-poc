b5403821cd679a8503aa26fe806baff13cc7b339
Jakub Cierlik <jakub.cierlik@amartus.com>
2021-04-27 08:58:29 +0200

    rds with certs - whole deployment merged

--- scripts/certificate.sh
+++ scripts/certificate.sh
@@ -10,8 +10,8 @@ sudo ln -s /snap/bin/certbot /usr/bin/certbot
 sudo certbot certonly --standalone --register-unsafely-without-email --agree-tos -d ${domain}
 cert=`sudo cat /etc/letsencrypt/live/${domain}/fullchain.pem`
 key=`sudo cat /etc/letsencrypt/live/${domain}/privkey.pem`
-cert_base64=`sudo cat /etc/letsencrypt/live/${domain}/fullchain.pem | base64`
-key_base64=`sudo cat /etc/letsencrypt/live/${domain}/privkey.pem | base64`
+cert_base64=`sudo cat /etc/letsencrypt/live/${domain}/fullchain.pem | base64 | tr -d "\n"`
+key_base64=`sudo cat /etc/letsencrypt/live/${domain}/privkey.pem | base64 | tr -d "\n"`
 ctx instance runtime-properties 'cert' "${cert}"
 ctx instance runtime-properties 'key' "${key}"
 ctx instance runtime-properties 'cert_base64' "${cert_base64}"
